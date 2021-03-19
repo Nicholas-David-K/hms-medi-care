d69b4a41054e32ab40cb9b1fbec2017d639a8c5e
Nicholas David <onedavid.nicholas@gmail.com>
2021-03-19 15:09:05 +0300

    Added Host

--- hms/settings.py
+++ hms/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '%@!$i!wk0t3qi6(x5vx5z*udfq26in5*v7ge!(cr(tro)z#4-+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['medicarehms.herokuapp.com']
 
 
 # Application definition
