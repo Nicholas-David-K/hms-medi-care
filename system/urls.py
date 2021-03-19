43c40756e7f1cc3521d60e138d3f30945f6a6863
Nicholas David <onedavid.nicholas@gmail.com>
2021-03-19 17:48:11 +0300

    updated url

--- system/urls.py
+++ system/urls.py
@@ -3,7 +3,7 @@ from .import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('create-appointment/', views.CreateAppointmentView.as_view(), name='create-appointment'),
+    path('appointment/create/', views.CreateAppointmentView.as_view(), name='create-appointment'),
     path('appointments/', views.AppointMentList.as_view(), name='appointment-list'),
     path('appointments/<int:pk>', views.AppointmentUpdateView.as_view(), name='appointment-detail'),
     path('appointments/<int:pk>/delete/', views.AppointmentDeleteView.as_view(), name='appointment-delete'),
